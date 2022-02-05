2124a761a91af357a2ba1502df59ae06675a7849
N. Federico Mahfoud <federicomahfoud@gmail.com>
2022-02-04 21:54:18 -0300

    feat: create database tables if not exists

--- app/server/Options/sDB.ts
+++ app/server/Options/sDB.ts
@@ -2,19 +2,23 @@ import * as mysql from 'mysql';
 import Logger from './sLogger';
 
 class Database {
-	connection: any;
+	private connection: mysql.Pool;
+	private config: mysql.ConnectionConfig;
 
 	constructor() {
-		this.connection = mysql.createPool({
+		this.config = {
 			host: process.env.MYSQL_HOST,
 			user: process.env.MYSQL_USER,
 			password: process.env.MYSQL_PASSWORD,
 			database: process.env.MYSQL_DATABASE
-		});
+		};
 
-		this.connection.getConnection((err: any) => {
+		Logger.info(`Trying to connect to database '${this.config.database}' as '${this.config.user}' at '${this.config.host}'...`);
+
+		this.connection = mysql.createPool(this.config);
+		this.connection.getConnection((err: mysql.MysqlError, connection: mysql.PoolConnection) => {
 			if (err) {
-				Logger.error(`MySQL server not working!`);
+				Logger.error(`The server can't connect to the database!`);
 				Logger.error(`err.code: ${err.code}`);
 				Logger.error(`err.errno: ${err.errno}`);
 				Logger.error(`err.fatal: ${err.fatal}`);
@@ -24,12 +28,13 @@ class Database {
 				throw err;
 			}
 			else {
-				Logger.info(`MySQL server ready!`);
+				Logger.info(`Connected to the database successfully.`);
+				this.createTables();
 			}
 		});
 	}
 
-	try(query: string) {
+	private try(query: string) {
 		return new Promise((r, j) => this.connection.query(query, null , (err: any, data: any) => {
 			if (err) {
 				Logger.error(query);
@@ -39,17 +44,54 @@ class Database {
 		}))
 	}
 
-	escape(value: any, stringifyObjects?: boolean, timeZone?: string): string {
+	public escape(value: any, stringifyObjects?: boolean, timeZone?: string): string {
 		return mysql.escape(value, stringifyObjects, timeZone);
 	}
 
-	async query(query: string) {
-		const start = new Date().getTime(); 
+	public async query(query: string) {
+		const start = new Date().getTime();
 		const data = await this.try(query);
 		const time = new Date().getTime() - start;
 		if (time >= 500) Logger.warn(`'${query}' ends with: ${time / 1000}s`);
 		else Logger.silly(`'${query}' ends with: ${time / 1000}s`);
 		return data;
+	}
+
+	private async createTables(): Promise<void> {
+		let tables = [
+			{
+				name: `users`,
+				queries: [
+					"CREATE TABLE `users` (" +
+						"`guid` int(255) NOT NULL," +
+						"`email` varchar(255) NOT NULL," +
+						"`firstName` varchar(255) NOT NULL," +
+						"`lastName` varchar(255) NOT NULL," +
+						"`socialClub` varchar(255) NOT NULL," +
+						"`lang` varchar(3) NOT NULL DEFAULT 'eng'," +
+						"`adminlvl` int(10) NOT NULL DEFAULT '0'," +
+						"`position` text NOT NULL," +
+						"`password` text NOT NULL," +
+						"`salt` text NOT NULL" +
+					") ENGINE=InnoDB DEFAULT CHARSET=utf8;",
+					"ALTER TABLE `users` ADD PRIMARY KEY (`guid`);",
+					"ALTER TABLE `users` MODIFY `guid` int(255) NOT NULL AUTO_INCREMENT;"
+				]
+			}
+		];
+
+		for (let table of tables) {
+			let result = await this.query(`SHOW tables FROM \`${this.config.database}\` LIKE '${table.name}';`);
+
+			if (!result[0]) {
+				Logger.warn(`'${table.name}' table not found! Trying to create...`);
+
+				for (let query of table.queries) {
+					Logger.warn(query);
+					await this.query(query);
+				}
+			}
+		}
 	}
 }
 const a = new Database();
