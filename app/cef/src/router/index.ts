0bd623bf9803a82d7f5b0e12a98a249601f3626f
N. Federico Mahfoud <federicomahfoud@gmail.com>
2022-03-15 11:25:41 -0300

    fix(cef): base url & router using hash

--- app/cef/src/router/index.ts
+++ app/cef/src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/Auth/LoginView.vue'
 import RegisterView from '@/views/Auth/RegisterView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
