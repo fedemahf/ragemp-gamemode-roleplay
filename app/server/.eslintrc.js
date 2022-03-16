f5ff9e617af5db40c1cde6fae7cdaadbc6586eaa
N. Federico Mahfoud <federicomahfoud@gmail.com>
2022-03-16 19:57:28 -0300

    build(npm): add eslint

---
+++ app/server/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // env: {
+  //   browser: true,
+  //   es2021: true
+  // },
+  extends: [
+    'standard-with-typescript'
+  ],
+  // parser: '@typescript-eslint/parser',
+  parserOptions: {
+    // ecmaVersion: 'latest',
+    // sourceType: 'module',
+    project: './tsconfig.json'
+  }
+}
